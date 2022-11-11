3de9f14a9c801ab9dc16a3a2a95072b4bd49fb48
Vince Pelss <vpelss@users.noreply.github.com>
2022-11-11 10:47:43 -0500

    recursive read

--- inc/pages.php
+++ inc/pages.php
@@ -2,7 +2,59 @@
 
 if ( ! defined( 'ABSPATH' ) ) {	exit($staus='ABSPATH not defn'); } //exit if directly accessed
 
-add_action( 'wp_loaded', 'read_and_create_pages' ); //maybe move to activate.php later
+add_action( 'wp_loaded', 'read_and_create_pages2' ); //maybe move to activate.php later
+
+function read_and_create_pages2(){
+	$dir = EMOGIC_TAROT_PLUGIN_PATH . "/pages";
+	$parent_id = 0;
+	add_pages($dir,$parent_id);
+}
+
+function add_pages($dir,$parent_id){
+	$files = array_diff(scandir($dir), array('..', '.'));
+	foreach ($files as $file) {
+		if(is_file($file)){
+			$r =88;
+			$parent_id_temp = post_page_if_required2();
+		}
+		if(is_dir($file)){
+			$r=76;
+			//create empty page
+			$parent_id_temp = post_page_if_required2();
+			add_pages($dir.'/'.$file , $parent_id_temp);
+		}
+
+		//$data = file_get_contents($dir . $file , true);
+		//$post_id = post_page_if_required( 0 , $file , '' , $dir , 1);
+		//if($post_id) wp_publish_post( $post_id );
+		}
+	return;
+}
+
+function post_page_if_required2( $post_parent_ID , $file , $path_parent , $dir , $load_data = 0){//$file is pagename, $path is page path with parents, $data is data for the page
+		/*
+		$path_file = $path_parent . '/' . $file;
+		$page_test = get_page_by_path($path_file); //post object or null
+		if( $page_test ){return 0;} //skip if this file page already exists
+		if($load_data){
+			$data = file_get_contents($dir . $file , true);
+		}
+		else{
+			$data = '';
+		}
+		//create
+		$wordpress_page = array(
+			//'ID' => $page_id,
+			'post_title'    => $file,
+			'post_name' => $file,
+			'post_content'  => $data,
+			'post_status'   => 'draft',
+			'post_type' => 'page',
+			'post_parent' => $post_parent_ID
+			);
+		return wp_insert_post( $wordpress_page );
+		*/
+}
 
 //load default pages and create them if they do not already exist/ place them as sub pages under main tarot page
 function read_and_create_pages(){
@@ -13,9 +65,6 @@ $files = scandir($dir);
 foreach ($files as $file) {
 		if($file == "."){continue;}
 		if($file == ".."){continue;}
-		//$data = file_get_contents($dir . $file , true);
-		//post_page_if_required( $post_parent_ID , $file , $path_parent , $data);
-		//post_page_if_required( $post_parent_ID , $file , $path_parent , $dir , 1);
 
 		$data = file_get_contents($dir . $file , true);
 		$post_id = post_page_if_required( 0 , $file , '' , $dir , 1);
